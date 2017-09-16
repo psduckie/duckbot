fead5feee540380ca269a298e05c0174a17fe51c
PSDuckie <psduckie@yahoo.com>
2017-09-16 15:06:31 -0400

    Added rudimentary functions to the bot

--- Duckbot/Program.cs
+++ Duckbot/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using DSharpPlus;
+using System.Threading.Tasks;
 
 namespace Duckbot
 {
     class Program
     {
+        static DiscordClient discord;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "MzU4Njc5MzM2MzY4NDcyMDY1.DJ79xQ.nSYXYLEopXHAJF-vno_OzOvcNgA",
+                TokenType = TokenType.Bot
+            });
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping")) await e.Message.RespondAsync("pong!");
+            };
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
